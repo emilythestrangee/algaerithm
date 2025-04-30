ab013dc79038c734c9e75d85dc1c45605ca2b303
emilythestrangee <88434520+emilythestrangee@users.noreply.github.com>
2025-04-30 17:32:04 +0300

    Update vite.config.js

--- ByteCurrent-frontend/vite.config.js
+++ ByteCurrent-frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     port: process.env.PORT || 4173,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ['algaerithm.onrender.com']
   }
 })
